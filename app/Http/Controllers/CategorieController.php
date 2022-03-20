8516098e1ee841325f79b7beacb83fb7d8a0cd1d
rouamansour <rouamansour.rm@gmail.com>
2022-03-20 12:36:29 +0000

    create category

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categories = Categorie::all()->toArray();
+        return array_reverse($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $categorie = new Categorie([
+            'nomcategorie' => $request->input('nomcategorie'),
+            'imagecategorie' => $request->input('imagecategorie')
+            ]);
+            $categorie->save();
+            return response()->json('Catégorie créée !');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $categorie = Categorie::find($id);
+        return response()->json($categorie);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        //
+        $categorie = Categorie::find($id);
+        $categorie->update($request->all());
+        return response()->json('Catégorie MAJ !');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $categorie = Categorie::find($id);
+        $categorie->delete();
+        return response()->json('Catégorie supprimée !');
+    }
+}
